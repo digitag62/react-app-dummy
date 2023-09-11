4dbb5bbab0626af5b29069a0128c06eaf7654a7a
digitag62 <144676268+digitag62@users.noreply.github.com>
2023-09-11 11:00:37 +0000

    hamburgrr

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import { Route, Routes } from "react-router-dom";
 import Navigation from "./routes/navigation/navigation.comp";
 import Home from "./routes/home/home.comp";
 import About from "./routes/about/about.comp";
 import Contact from "./routes/contact/contact.comp";
-
-// import './App.css'
 
 function App() {
   const [count, setCount] = useState(0);
--- src/routes/navigation/navigation.comp.jsx
+++ src/routes/navigation/navigation.comp.jsx
@@ -4,32 +4,82 @@ import { Link, Outlet } from "react-router-dom";
 const Navigation = () => {
   return (
     <>
-      <header className="flex w-full justify-center bg-transparent">
-        <div className="container flex items-center justify-between">
-          <div className="px-4">
-            <Link to="/" className="block py-6 text-lg font-bold text-primary">
-              Navs<span className="animate-ping">_</span>
-            </Link>
-          </div>
-          <div className="flex items-center px-4">
-            <ul className="flex">
-              <li className="group">
-                <Link
-                  to="about"
-                  className="mx-8 flex py-2 group-hover:text-primary"
-                >
-                  About
-                </Link>
-              </li>
-              <li className="group">
-                <Link
-                  to="contact"
-                  className="mx-8 flex py-2 group-hover:text-primary"
-                >
-                  Contact
-                </Link>
-              </li>
-            </ul>
+      <header class="bg-trasparent absolute top-0 left-0 w-full flex items-center z-10">
+        <div class="container">
+          <div class="flex items-center justify-between relatif">
+            <div class="px-4">
+              <a href="#home" class="font-bold text-xl text-primary block py-4">
+                DevsTags<span className="animate-ping">_</span>
+              </a>
+            </div>
+            <div class="flex items-center px-4">
+              <button
+                id="hamburger"
+                name="hamburger"
+                type="button"
+                class="block absolute right-4 lg:hidden"
+              >
+                <span class="hamburger-line transition duration-300 ease-in-out origin-top-left"></span>
+                <span class="hamburger-line transition duration-300 ease-in-out"></span>
+                <span class="hamburger-line transition duration-300 ease-in-out origin-bottom-left"></span>
+              </button>
+
+              <nav
+                id="nav-menu"
+                class="hidden absolute py-5 bg-white shadow-xl rounded-lg max-w-[200px] w-full right-4 top-full lg:block lg:static lg:bg-transparent lg:max-w-full lg:shadow-none lg:rounded-none"
+              >
+                <ul class="block lg:flex">
+                  <li class="group">
+                    <a
+                      href="#home"
+                      class="text-base text-dark py-2 mx-8 flex group-hover:text-primary"
+                    >
+                      Beranda
+                    </a>
+                  </li>
+                  <li class="group">
+                    <a
+                      href="#About"
+                      class="text-base text-dark py-2 mx-8 flex group-hover:text-primary"
+                    >
+                      About
+                    </a>
+                  </li>
+                  <li class="group">
+                    <a
+                      href="#Portfolio"
+                      class="text-base text-dark py-2 mx-8 flex group-hover:text-primary"
+                    >
+                      Portfolio
+                    </a>
+                  </li>
+                  <li class="group">
+                    <a
+                      href="#Clients"
+                      class="text-base text-dark py-2 mx-8 flex group-hover:text-primary"
+                    >
+                      Client
+                    </a>
+                  </li>
+                  <li class="group">
+                    <a
+                      href="#blog"
+                      class="text-base text-dark py-2 mx-8 flex group-hover:text-primary"
+                    >
+                      Blog
+                    </a>
+                  </li>
+                  <li class="group">
+                    <a
+                      href="#Contact"
+                      class="text-base text-dark py-2 mx-8 flex group-hover:text-primary"
+                    >
+                      Contact
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
           </div>
         </div>
       </header>
